2c4612b4c9d7e4bcf249253913c34038008e52a7
Zhenhuan Liu <nkulzh16@gmail.com>
2022-09-04 10:20:16 -0700

    Add sample script to generate class images.

---
+++ sample.py
@@ -0,0 +1,26 @@
+from diffusers.pipelines import StableDiffusionPipeline
+import torch
+
+sample_nums = 200
+batch_size = 16
+prompt = "a photo of dog"
+save_dir = "data/dogs/class"
+
+
+
+if __name__ == "__main__":
+    model_id = "CompVis/stable-diffusion-v1-4"
+    device = "cuda"
+    model = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True, cache_dir="./.cache").to(device)
+
+    datasets = [prompt] * sample_nums
+    datasets = [datasets[x:x+batch_size] for x in range(0, sample_nums, batch_size)]
+    id = 0
+
+    for text in datasets:
+        with torch.no_grad():
+            images = model(text, height=512, width=512, num_inference_steps=50)["sample"]
+
+        for image in images:
+            image.save(f"{save_dir}/{id}.png")
+            id += 1
